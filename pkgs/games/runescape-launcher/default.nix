ec9dfec339aaddb2e1632eb06f104582113929b0
Sara Johnsson <46252070+Fendse@users.noreply.github.com>
2020-12-25 18:55:33 +0100

    runescape-launcher: remove some unneeded dependencies

--- pkgs/games/runescape-launcher/default.nix
+++ pkgs/games/runescape-launcher/default.nix
@@ -10,20 +10,13 @@ buildFHSUserEnv rec {
     xorg.libSM
     xorg.libX11
     xorg.libXxf86vm
-    libpng12
     glib
-    glib_networking
     pango
     cairo
     gdk_pixbuf
-    curl
     gtk2
-    expat
     SDL2
     zlib
-    glew110
-    mesa
-    firefox
     xdg_utils
     libglvnd
 
