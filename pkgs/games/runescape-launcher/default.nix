a54852881532b72004dffa8f3bbda7750e350852
Sara Johnsson <46252070+Fendse@users.noreply.github.com>
2020-12-27 16:06:12 +0100

    runescape-launcher: add meta and fix a bug in the .desktop file

--- pkgs/games/runescape-launcher/default.nix
+++ pkgs/games/runescape-launcher/default.nix
@@ -1,26 +1,47 @@
-# Stolen from github.com/evanjs/nixos_cfg
-{ stdenv, callPackage, buildFHSUserEnv }:
+{ buildFHSUserEnvBubblewrap, callPackage, lib,
+  glib, pango, cairo, gdk_pixbuf, gtk2, SDL2, zlib,
+  xdg_utils, libglvnd, libcap, openssl, xorg,
+  libSM ? xorg.libSM, libX11 ? xorg.libX11, libXxf86vm ? xorg.libXxf86vm
+}:
+let
+  runtime = callPackage ./runtime.nix { };
+  pname = "runescape-launcher";
+  version = runtime.version;
+  name = "${pname}-${version}";
+in
+  buildFHSUserEnvBubblewrap {
+    inherit name;
 
-buildFHSUserEnv rec {
-  name = "runescape-launcher";
+    runScript = ''${runtime}/bin/runescape-launcher'';
 
-  runScript = "${callPackage ./runtime.nix {}}/bin/runescape-launcher";
+    extraInstallCommands = ''
+      cp -r -- ${runtime}/share "$out"
+      mv -- "$out/bin/${name}" "$out/bin/${pname}"
+      substituteInPlace "$out/share/applications/runescape-launcher.desktop" --replace '/usr/bin/' ""
+    '';
 
-  targetPkgs = pkgs: with pkgs; [
-    xorg.libSM
-    xorg.libX11
-    xorg.libXxf86vm
-    glib
-    pango
-    cairo
-    gdk_pixbuf
-    gtk2
-    SDL2
-    zlib
-    xdg_utils
-    libglvnd
+    targetPkgs = lib.const [
+      libSM
+      libX11
+      libXxf86vm
+      glib
+      pango
+      cairo
+      gdk_pixbuf
+      gtk2
+      SDL2
+      zlib
+      xdg_utils
+      libglvnd
+      libcap
+      openssl
+    ];
 
-    libcap
-    openssl
-  ];
-}
+    meta = with lib; {
+      description = "A free MMORPG from Jagex LTD";
+      homepage = "https://runescape.com";
+      license = licenses.unfree;
+      maintainers = maintainers.Fendse;
+      platforms = [ "x86_64-linux" ]; # TODO: Figure out what other platforms might work
+    };
+  }
